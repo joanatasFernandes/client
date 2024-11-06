4e1e28df119e59f03a589953d2e86383ac1d482d
Joanatas Fernandes Barbosa <94496092+joanatasFernandes@users.noreply.github.com>
2024-11-06 15:55:04 -0300

    Update ClientApplication.java

--- src/main/java/com/clientapi/ClientApplication.java
+++ src/main/java/com/clientapi/ClientApplication.java
@@ -1,9 +1,12 @@
 package com.clientapi;
 
+import com.clientapi.config.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ApplicationProperties.class})
 public class ClientApplication {
 
 	public static void main(String[] args) {
